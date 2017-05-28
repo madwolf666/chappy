8b79a988ac0f6716869d7cc5140ec2aaff6eb2c5
Hitohi Nishimura <madwolf666@live.jp>
2017-05-28 22:05:52 +0900

    loto7

--- prediction_loto7.py
+++ prediction_loto7.py
@@ -2,60 +2,49 @@ import codecs
 from pprint import pprint
 import numpy as np
 import matplotlib.pylab as plt
+from mct_functions import sigmoid, softmax
 
-_learned_data1 = np.array([])
-_learned_data2 = np.array([])
-_learned_data3 = np.array([])
-_learned_data4 = np.array([])
-_learned_data5 = np.array([])
-_learned_data6 = np.array([])
-_learned_data7 = np.array([])
+_learned_data = [[] for j in range(7)]
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 def _read_learned_data():
-    fin = codecs.open('C:\\Users\\hal\\Downloads\\loto7.csv', 'r', 'shift_jis')
+    fin = codecs.open('C:\\Users\\ict\\Downloads\\loto7.csv', 'r', 'shift_jis')
     a_rec = 0
     for a_line in fin:
         a_split= a_line.replace("'", "").split(",")
         a_rec+=1
         #1行目はタイトルの為、読み飛ばす
         if (a_rec >= 2):
-            _learned_data1 = np.append(_learned_data1, np.array(a_split[2]))
-            _learned_data2 = np.append(_learned_data2, np.array(a_split[3]))
-            _learned_data3 = np.append(_learned_data3, np.array(a_split[4]))
-            _learned_data4 = np.append(_learned_data4, np.array(a_split[5]))
-            _learned_data5 = np.append(_learned_data5, np.array(a_split[6]))
-            _learned_data6 = np.append(_learned_data6, np.array(a_split[7]))
-            _learned_data7 = np.append(_learned_data7, np.array(a_split[8]))
+            for a_num in range(7):
+                _learned_data[a_num].append(int(a_split[a_num + 2], 10))
 
-    print('*** 第1数字の当選結果 ***')
-    pprint(_learned_data1)
-    print('*** 第2数字の当選結果 ***')
-    pprint(_learned_data2)
-    print('*** 第3数字の当選結果 ***')
-    pprint(_learned_data3)
-    print('*** 第4数字の当選結果 ***')
-    pprint(_learned_data4)
-    print('*** 第5数字の当選結果 ***')
-    pprint(_learned_data5)
-    print('*** 第6数字の当選結果 ***')
-    pprint(_learned_data6)
-    print('*** 第7数字の当選結果 ***')
-    pprint(_learned_data7)
+    for a_num in range(7):
+        print('*** 第' + str(a_num + 1) + '数字の当選結果 ***')
+        print(len(_learned_data[a_num]))
+        pprint(_learned_data[a_num])
 
-def _sigmoid(n):
-    print('***********************')
-    Y = sigmoid(_learned_data1)
-    print(Y)
-    plt.plot(_learned_data1, Y)
-    #plt.ylim(-0.1, 1.1)
+def _transition():
+    plt.figure(figsize=(30, 20))
+
+    for a_num in range(7):
+        a_x = np.arange(0, len(_learned_data[a_num]), 1)
+        a_idx = 1
+        if (a_num > 2) and (a_num < 6):
+            a_idx = 2
+        else:
+            a_idx = 3
+        plt.subplot(4, 3, a_num + 1)
+        plt.plot(a_x, _learned_data[a_num])
+        plt.ylim(1, 37)
+        plt.title('No.' + str(a_num + 1))
     plt.show()
 
 if __name__ == '__main__':
     #過去の当選結果を読み込む
     _read_learned_data()
     #第n数字毎に推移を検証
-    _sigmoid(1)
+    _transition()
     #第n数字毎に予測を検証
+
